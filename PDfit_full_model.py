af764bf3898e2a53a2f395b8d0126f705a2c1ade
caro-veg <52160708+caro-veg@users.noreply.github.com>
2019-11-15 18:40:07 +0000

    Add files via upload

--- PDfit_full_model.py
+++ PDfit_full_model.py
@@ -30,56 +30,49 @@ df_control.columns = ['Strain', 'Rep', 'Agent', 'Conc', 'hr0', 'hr2', 'hr4', 'hr
 #df['Agent'].unique()
 
 # extract observation
-df_ag_strain = df[(df['Strain']=='100042BU') & (df['Agent']=='Gepotidacin')]
-df_ag_strain_control = df_control[(df['Strain']=='100042BU') & (df['Agent']=='Gepotidacin')]
-dfc = df_ag_strain_control[0:5]
-dfc['Conc'] = [0] * 5
-df_ag_strain = dfc.append(pd.DataFrame(data=df_ag_strain), ignore_index=True)
+df_ag_strain = df[(df['Strain']=='100042BU') & (df['Agent']=='Gepotidacin') & (df['Conc']==0.03)]
+#df_ag_strain_control = df_control[(df['Strain']=='100042BU') & (df['Agent']=='Gepotidacin')] 
+#dfc = df_ag_strain_control[0:5]
+#dfc['Conc'] = [0.] * 5
+#df_ag_strain = dfc.append(pd.DataFrame(data=df_ag_strain), ignore_index=True)
 logobs = np.asarray(df_ag_strain[['hr0', 'hr2', 'hr4', 'hr8']])
 obs = 10 ** logobs
-#obs_array = np.array(obs)
 
 
 # time over which growth occurs
 times = np.array([0., 2., 4., 8.])
-# repetitions of each experiment (Agent/Strain combination)
-num_reps = 5
+
 
 plt.plot(times, logobs.T)
 
+
 # define priors
-#r = np.random.uniform(low=-1., high=1.)
-log10_K = np.random.uniform(low=0., high=6.)
-K = 10**log10_K
-rmax = np.random.uniform(low=0., high=1.)
-s = np.random.uniform(low=-2., high=0.)
-#params = np.array([r, K, rmax, s])
-params = np.array([rmax, s, K])
+a = np.random.uniform(low=-10., high=10.)
+s = np.random.uniform(low=-10., high=10.)
+q = np.random.uniform(low=-10., high=10.)
+params = np.array([a, s, q])
+
+
+
 
 C = df_ag_strain['Conc'].to_numpy()
+B0 = 10 ** df_ag_strain['hr0'].to_numpy()
+rmax = np.array([0.21048, 0.21048, 0.21048, 0.21048, 0.21048])
 
-# define model
-def growth_conc(params, C):
-    rmax = params[0]
-    s = params[1]
-    #print(s)
-    return rmax * np.exp(s * C)
 
-def exp_growth(params, C, times):
-    K = params[2]
-    r = growth_conc(params, C)
-    #print(r)
-    return K * np.exp(np.transpose(np.array([r,])) * times)
-
+def exp_growth(params, C, B0, rmax, times):
+    a = params[0]
+    s = params[0]
+    q = params[2]
+    r = a - np.exp(s * (C + q))
+    return np.transpose(np.array([B0,])) * np.exp(np.transpose(np.array([r,])) * times)
     
 
 # define log likelihood
-def log_like(params, times, C, d):
-    B = exp_growth(params, C, times)
+def log_like(params, times, C, B0, rmax, d):
+    B = exp_growth(params, C, B0, rmax, times)
     B = np.log10(B)
-    #print(B)
     ll = np.sum(np.log(stats.norm.pdf(d, B)))
-    #print(ll)
     if not np.isfinite(ll):
         return -np.inf    
     return ll
@@ -87,44 +80,42 @@ def log_like(params, times, C, d):
 # determine starting values from ML fitting
 nll = lambda *args: - log_like(*args)
 initial = np.array(params)
-soln = minimize(nll, initial, args=(times, df_ag_strain['Conc'], logobs))
+soln = minimize(nll, initial, args=(times, C, B0, rmax, logobs))
 
 
 # define prior probability
 def log_prior(params):
-    rmax, s, K = params
-    if (0. < rmax < 1.) and (-2. < s < 0.) and (0. < K < 1e6):
+    a, s, q = params
+    if (-10. < a < 10.) and (-10. < s < 10.) and (-10. < q < 10):
         return 0
-    #else:
-        #print("inf")
     return -np.inf
 
 # define log probability using prior probability and log likelihood
-def log_prob(params, times, C, d):
+def log_prob(params, times, C, B0, rmax, d):
     lp = log_prior(params)
     if not np.isfinite(lp):
         return -np.inf
-    return lp + log_like(params, times, C, d)
+    return lp + log_like(params, times, C, B0, rmax, d)
 
 
 # draw initial values using ML results
-nwalkers = 6
+nwalkers = 8
 ndim = len(params)
 params0 = soln.x + 1e-2 * np.random.randn(nwalkers, ndim)
 
    
 # define sampler
-sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, args=(times, C, logobs))
+sampler = em.EnsembleSampler(nwalkers, ndim, log_prob, args=(times, C, B0, rmax, logobs))
 
 # run MCMC
-state = sampler.run_mcmc(params0, 10000, progress=True)
+state = sampler.run_mcmc(params0, 100000, progress=True)
 
 # get MCMC chain for downstream analysis
 samples = sampler.get_chain()
 
 # plot chains for all fitted parameters
 fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
-labels = ['r_max', 's', 'K']
+labels = ['a', 's', 'q']
 for i in range(ndim):
     ax = axes[i]
     ax.plot(samples[:, :, i], 'k', alpha=0.3)
@@ -133,6 +124,10 @@ for i in range(ndim):
 axes[-1].set_xlabel('step number')
 #fig.savefig('C:\\Users\\cvegvari\\Documents\\Python Scripts\\PDfit\\chains_-Ciprofloxacin-4.0.pdf', dpi=300, bbox_inches='tight')
 #plt.close(fig)
+
+#plt.plot(samples[:, :, 0], "k", alpha=0.3)
+#plt.xlim(0, len(samples))
+#plt.xlabel("step number")
 
 
 # check autocorrelation
@@ -154,8 +149,8 @@ r_95pc = np.percentile(flat_samples[:,0], [5, 95])
 s_median= np.median(flat_samples[:,1])
 s_95c = np.percentile(flat_samples[:,1], [5, 95])
 
-K_median = np.median(flat_samples[:,2])
-K_95pc = np.percentile(flat_samples[:,2], [5, 95])
+q_median = np.median(flat_samples[:,2])
+q_95pc = np.percentile(flat_samples[:,2], [5, 95])
 
 #f = open('C:\\Users\\cvegvari\\Documents\\Python Scripts\\PDfit\\median_95pc_100058BU-Ciprofloxacin-4.0.txt', 'w')
 #print('r median (95 percentile):', r_median, ' (', r_95pc[0], ', ', r_95pc[1], ')', file=f)
